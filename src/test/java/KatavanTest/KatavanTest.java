b292787526fb905dcdc9edcf1bbfee2504268221
makatt <loshamaks@gmail.com>
2024-12-19 14:33:51 +0300

    update allure

--- src/test/java/KatavanTest/KatavanTest.java
+++ src/test/java/KatavanTest/KatavanTest.java
@@ -1,19 +1,29 @@
 package KatavanTest;
-import org.junit.Assert;
+
 import org.junit.Test;
 import allclasses.katavan.Katavan;
 
+import io.qameta.allure.junit4.DisplayName;
+import io.qameta.allure.Description;
+import io.qameta.allure.Step;
+import io.qameta.allure.TmsLink;
+import io.qameta.allure.Issue;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThrows;
 
 public class KatavanTest {
 
     @Test
+    @DisplayName("test")
+    @Description("test")
+    @TmsLink("Testcase-231")
+    @Issue("BUG-34")
     public void testFactorialOfZero() {
         assertEquals(1, Katavan.factorial(0));
     }
 
     @Test
+    @Step("1 step")
     public void testFactorialOfPositiveNumber() {
         assertEquals(120, Katavan.factorial(5)); // 5! = 120
         assertEquals(720, Katavan.factorial(6)); // 6! = 720
