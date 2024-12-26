737ded6a8ecb1aee834653e0e0690d0e8107f574
Лиза Покровская <El@jw>
2024-12-27 00:29:16 +0300

    Добавление своего класса

--- src/test/java/PokrovskayaTest/PokrovskayaTest.java
+++ src/test/java/PokrovskayaTest/PokrovskayaTest.java
@@ -1,13 +1,17 @@
 package PokrovskayaTest;
 
 import allclasses.Pokrovskaya.PokrovClass;
+//import jdk.jfr.Description;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
 
 public class PokrovskayaTest {
 
     // Тест для метода start()
     @Test
+    @DisplayName("Проверка метода start")
+    //@Description("Транспорт типа Автомобиль запускается.")
     public void getStart() {
         // Создаем объект транспорта
         PokrovClass pokrovClass = new PokrovClass("Автомобиль", 4);
@@ -17,15 +21,18 @@ public class PokrovskayaTest {
 
     // Тест для метода stop()
     @Test
+    @DisplayName("Проверка метода stop")
+    //@Description("Транспорт типа Велосипед остановлен.")
     public void testStop() {
         // Создаем объект транспорта
         PokrovClass pokrovClass = new PokrovClass("Велосипед", 1);
 
-        Assert.assertEquals("Транспорт типа Велосипед остановлен.", pokrovClass.start());
+        Assert.assertEquals("Транспорт типа Велосипед остановлен.", pokrovClass.stop());
     }
 
     // Тест для проверки значений полей type и capacity
     @Test
+    //@DisplayName("Проверка значений полей type и capacity")
     public void testTransportFields() {
         // Создаем объект транспорта
         PokrovClass pokrovClass = new PokrovClass("Автомобиль", 5);
