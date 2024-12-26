e5b64625ca334bde223b5dc411c42282c878f63a
Nikita <filipnekit16418@gmail.com>
2024-12-27 02:04:53 +0300

    filippov add foxClasses

--- src/test/java/filippovTest/foxTest.java
+++ src/test/java/filippovTest/foxTest.java
@@ -21,7 +21,7 @@ public class foxTest {
     @Test
     public void setHeightNormalHeightEqualsGetHeight() throws Exception{
         Fox foxy = new Fox();
-        double testHeight = 40;
+        double testHeight = 40.0;
         foxy.setHeight(testHeight);
         Assert.assertEquals(testHeight, foxy.getHeight());
     }
