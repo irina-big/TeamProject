b9a8bfff68ccfcc223f704d733ca8612a7886473
antiqarck <filipnekit16418@gmail.com>
2024-12-26 23:50:50 +0300

    filippov add foxClasses

--- src/test/java/filippovTest/foxTest.java
+++ src/test/java/filippovTest/foxTest.java
@@ -1,4 +1,29 @@
 package filippovTest;
 
+import allclasses.filippov.Fox;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class foxTest {
+    @Test
+    public void getHuntingRangReturnF(){
+        Fox foxy = new Fox();
+        var lowRank = "F";
+        Assert.assertEquals(lowRank, foxy.getHuntingRang());
+    }
+    @Test
+    public void upHuntingRangToMaxReturnP(){
+        Fox foxy = new Fox();
+        var maxRank = "P";
+        while(foxy.getHuntingRang() != foxy.upHuntingRang())
+            foxy.upHuntingRang();
+        Assert.assertEquals(maxRank, foxy.getHuntingRang());
+    }
+    @Test
+    public void setHeightNormalHeightEqualsGetHeight(){
+        Fox foxy = new Fox();
+        var testHeight = 40;
+        foxy.setHeight(testHeight);
+        Assert.assertEquals(testHeight, foxy.getHeight());
+    }
 }
